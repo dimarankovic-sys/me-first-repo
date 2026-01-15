3ab45734a114a28e68444a1de4a170adefe78947
dimarankovic-sys <dima.rankovic@gmail.com>
2026-01-15 20:19:45 +0100

    Create sales_data.py

---
+++ sales_data.py
@@ -0,0 +1,13 @@
+#Dimitrije Rankovic
+
+sales_data = [230, 200, 310, 290, 400, 150, 180]
+
+def total_sales(data):
+    return sum(data)
+
+def average_sales(data):
+    return sum(data) / len(data)
+
+print(f"Sales data for the week: {sales_data}")
+print(f"Total sales: {total_sales(sales_data)}")
+print(f"Average sales: {average_sales(sales_data)}")
